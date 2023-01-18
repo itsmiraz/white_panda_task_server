0740630a58996a00312f61993cc730dcbebe2082
itsmiraz <mirajulislamofficial044@gmail.com>
2023-01-18 11:47:54 +0600

    adding some more

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ app.use(express.json())
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fpgnyx0.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -25,40 +24,75 @@ async function run() {
             const result = await carsCollection.find(query).toArray()
             res.send(result)
         })
-        app.get('/carDetail/:id', async (req, res) => {
+        app.get('/carDetails/:id', async (req, res) => {
             const id = req.params.id
             const query = { _id: ObjectId(id) }
             const result = await carsCollection.findOne(query)
             res.send(result)
         })
+        app.get('/carDetail/:model', async (req, res) => {
+            const model = req.params.model
+            const query = { title: model }
+            const result = await carsCollection.findOne(query)
+            res.send(result)
+        })
+      
         app.put('/addbooking/:id', async (req, res) => {
             const id = req.params.id
-            const filter = {_id:ObjectId(id)}
-            const body = req.body
-            const option = { upsert: true }
+            const filter = { _id: ObjectId(id) }
+
+            const carDetail = await carsCollection.findOne(filter)
+            if (!carDetail.available) {
+                return res.send("Car Is Already Booked")
+            }
+            else {
+                const body = req.body
+                const option = { upsert: true }
+                const updateDoc = {
+                    $set: {
+                        available: false,
+                        bookedUser: body
+                    }
+                }
+
+                const result = await carsCollection.updateOne(filter, updateDoc, option)
+                res.send(result)
+            }
+
+
+        })
+        app.put('/updatebooking/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {
+                _id:ObjectId(id)
+            }
+       
+            const body = req.body;
             const updateDoc = {
                 $set: {
-                    available: false,
                     bookedUser:body
                 }
             }
-
-            const result = await carsCollection.updateOne(filter, updateDoc, option)
+            const option = { upsert: true }
+            const result = await carsCollection.updateOne(query, updateDoc, option)
             res.send(result)
         })
+        app.delete('/deletebooking/:id', async (req, res) => {
+            const id = req.params.id
+            console.log(id)
+            const query = { _id: ObjectId(id) }
+            const option = {upsert:true}
+            const updateDoc = {
+                $set: {
+                    available: true,
+                    bookedUser:{}
+                }
+            }
+            const result = await carsCollection.updateOne(query, updateDoc, option)
+            res.send('Booking Deleted')
+        })
 
-        // app.get('/test', async (req, res) => {
-        //     const query = {}
-        //     const option = { upsert: true }
-        //     const updateDoc = {
-        //         $set: {
-        //             available: true,
-        //             bookedUser:{}
-        //         }
-        //     }
-        //     const result = await carsCollection.updateMany(query, updateDoc, option)
-        //     res.send(result)
-        // })
+      
 
     }
     finally {
